e81ae777466166a76b85b4e410a9f0d8bed0476d
Bas Kooijman <newcityofatlantis@gmail.com>
2023-09-18 08:08:57 +0200

    initial

---
+++ vendor/composer/installed.php
@@ -0,0 +1,34 @@
+<?php return array(
+    'root' => array(
+        'name' => 'atlantisgroup/ormag-bundle',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => 'a5df6901c2bca5809571a289eda4a5e89a3470fd',
+        'type' => 'ormag-bundle',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'atlantisgroup/ormag' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => 'f2f2ae82cd64784d3def55065f7a74e0630b029f',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../atlantisgroup/ormag',
+            'aliases' => array(
+                0 => '9999999-dev',
+            ),
+            'dev_requirement' => false,
+        ),
+        'atlantisgroup/ormag-bundle' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => 'a5df6901c2bca5809571a289eda4a5e89a3470fd',
+            'type' => 'ormag-bundle',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
